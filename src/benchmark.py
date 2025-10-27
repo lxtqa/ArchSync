43b2beb210330b014039ca4d61eb4351fae69910
lxtqa <1270824151@qq.com>
2025-10-27 18:47:08 +0800

    add benchmark

---
+++ src/benchmark.py
@@ -0,0 +1,193 @@
+import os
+import re
+from utils.arch_utils import *
+from utils.ast_utils import *
+from utils.patch_utils import *
+from block_result import block_result
+from extract_block import extract_block
+import json
+import tempfile
+import concurrent.futures
+from time import time
+from gen_result import gen_result
+import copy
+
+
+
+def construct_mapping_dic(matches):
+    match_dic = {}
+    for match in matches:
+        if match[1]!="---":
+            exit(201)
+        match_dic[match[2]] = match[3]
+    mapping_dic = {}
+    for key in match_dic.keys():
+        value = match_dic[key]
+        if key.split(" ")[0] == "name:" and value.split(" ")[0] == "name:":
+            k,v = get_name(key),get_name(value)
+            if k not in mapping_dic.keys():
+                mapping_dic[k] = {v:1}
+            else:
+                if v not in mapping_dic[k].keys():
+                    mapping_dic[k][v] = 1
+                else:
+                    mapping_dic[k][v] = mapping_dic[k][v] + 1
+    return mapping_dic
+
+
+def construct_matches(file1,file2):
+    if file1 == {} or file2 == {} or file1 == "" or file2 == "" or  file1 == file2:
+        return []
+    if type(file1) != str:
+        file1 = "\n".join(file1["content"])
+    if type(file2) != str:
+        file2 = "\n".join(file2["content"])
+    with tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as cfile1, \
+        tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as cfile2:
+        cfile1.write(file1)
+        cfile1.flush()
+        cfile2.write(file2)
+        cfile2.flush()
+        result = subprocess.run(["gumtree","textdiff",cfile1.name, cfile2.name,
+                                "-m",MATCHER_ID,"-g", TREE_GENERATOR_ID],
+                                stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        matches, _ = gumtree_parser(result.stdout.decode())
+    return matches
+
+
+
+
+
+def remove_cpp_comments(file_content):
+    pattern = r'//.*?$|/\*.*?\*/'
+    cleaned_content = re.sub(pattern, '', file_content, flags=re.DOTALL | re.MULTILINE)
+    return cleaned_content
+
+def file_result(file1,patch1,file2,use_docker,MATCHER_ID,TREE_GENERATOR_ID,mapping_dic):
+    patch1 = copy.deepcopy(patch1)
+    with tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as cfile1, \
+        tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.patch') as patchfile1:
+            cfile1.write(file1)
+            cfile1.flush()
+            patchfile1.write("\n".join(patch1["patch"])+"\n")
+            patchfile1.flush()
+            output11_ = subprocess.run(["patch",cfile1.name,patchfile1.name,"--output=-"],capture_output=True,text = True)
+            file1_string_std = output11_.stdout
+    return gen_result(file1,file2,file1_string_std,mapping_dic=mapping_dic,use_docker=use_docker,MATCHER_ID=MATCHER_ID,TREE_GENERATOR_ID=TREE_GENERATOR_ID)
+
+
+def successfully_generate(item1,item2,mapping_dic,r,i,j):
+    if item1 == [] or item2 == [] or item1 == item2:
+        return None
+    try:
+        file1,patch1 = item1
+        file2,patch2 = item2
+
+        modify_hex(file1)
+        modify_hex(file2)
+
+        with tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as cfile2, \
+            tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.patch') as patchfile2:
+                cfile2.write(file2)
+                cfile2.flush()
+                patchfile2.write("\n".join(patch2["patch"])+"\n")
+                patchfile2.flush()
+                output22_ = subprocess.run(["patch",cfile2.name,patchfile2.name,"--output=-"],capture_output=True,text = True)
+                file2_string_std = output22_.stdout
+        use_docker = False
+        file2_string = block_result(file1,patch1,file2,use_docker=use_docker,MATCHER_ID=MATCHER_ID,TREE_GENERATOR_ID=TREE_GENERATOR_ID,mapping_dic = mapping_dic)
+        clean_file2_string_std = remove_whitespace(remove_cpp_comments(format(file2_string_std)))
+        clean_file2_string = remove_whitespace(remove_cpp_comments(format(file2_string)))
+        if clean_file2_string_std == clean_file2_string:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+
+def main():
+    with open("test/diff.json") as jsonFile:
+        versions_diff_block = json.load(jsonFile)
+        for v,version in enumerate(versions_diff_block):
+            vresult = []
+            dir = "./v8"
+            os.chdir(dir)
+            os.system("git -c advice.detachedHead=false  checkout " + version["versions"][0])
+            print()
+            type_map = {}
+            for t,file_type in enumerate(version["contents"]):
+                for ft in file_type:
+                    if ft!={}:
+                        if remove_archwords(ft["file"]) not in type_map.keys():
+                            type_map[remove_archwords(ft["file"])] = [file_type]
+                        else:
+                            type_map[remove_archwords(ft["file"])].append(file_type)
+                        break
+
+            len_item = 0
+            total_tasks = 0
+            succeed_tasks = 0
+            start_time = time()
+            for t,file_type in enumerate(type_map.keys()):
+                mapping_dics = [[{} for _ in range(len(arch_dic))] for _ in range(len(arch_dic))]
+                contents = ["" for _ in range(len(arch_dic))]
+
+                for files in type_map[file_type]:
+                    files = files[0:4] + files[5:]
+                    for r,file in enumerate(files):
+                        if file != {} and contents[r] == "":
+                            with open(file["file"],"r") as f:
+                                contents[r] = modify_hex(format(f.read(),".."))
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future_to_task = {executor.submit(construct_matches, contents[i], contents[2]): (i, 2)
+                                        for i in range(len(contents))}
+                    for future in concurrent.futures.as_completed(future_to_task):
+                        i,j = future_to_task[future]
+                        try:
+                            mapping_dics[i][j] = construct_mapping_dic(future.result())
+                        except Exception as e:
+                            pass
+
+                items = []
+                for r,files in enumerate(type_map[file_type]):
+                    item = []
+                    for file in files:
+                        if file == {}:
+                            item.append([])
+                            continue
+                        with open(file["file"],"r") as f:
+                            content = format(f.read(),"..")
+                            patch = {"header":file["header"],"patch":file["patch"]}
+                            item.append([content,patch])
+                    items.append(item)
+                results = [[0 for _ in range(len(arch_dic))] for _ in range(len(items))]
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future_to_task = {executor.submit(successfully_generate, items[r][i], items[r][2],mapping_dics[i][2],r+len_item,i,j ): (r, i, 2)
+                                    for r in range(len(items))
+                                    for i in range(len(arch_dic))
+                                    }
+                    for future in concurrent.futures.as_completed(future_to_task):
+                        r,i,j = future_to_task[future]
+                        succeed = future.result()
+                        if succeed == True:
+                            results[r][i] =  1
+                            succeed_tasks += 1
+                            total_tasks += 1
+                        elif succeed == False:
+                            results[r][i] =  -1
+                            total_tasks += 1
+                        else:
+                            results[r][i] = 0
+                vresult.extend(results)
+                len_item = len_item + len(items)
+            print("")
+            os.system("git -c advice.detachedHead=false checkout main > /dev/null 2>&1")
+            print()
+            os.chdir("..")
+
+            print("准确率: {}/{} = {}".format(succeed_tasks,total_tasks,succeed_tasks/total_tasks))
+            print("生成结果耗时: {}s".format(int(time()-start_time)))
+if __name__ == "__main__":
+    main()
---
+++ src/block_result.py
@@ -0,0 +1,142 @@
+from gen_result import gen_result
+import tempfile
+import subprocess
+from extract_block import extract_block
+import subprocess
+from utils.arch_utils import *
+from utils.patch_utils import *
+import copy
+
+
+def collect_mirrored_blocks(inner_headers,blocks2):
+
+    same_named = [False] * len(blocks2)
+    blocks2_name = [None] * len(blocks2)
+    for i in range(len(blocks2)):
+        blocks2_name[i] = extract_name(blocks2[i]['header'])
+    for i in range(len(blocks2)):
+        if same_named[i]:
+            continue
+        name1 = blocks2_name[i]
+        if name1 == None:
+            continue
+        for j in range(i+1,len(blocks2)):
+            if same_named[j]:
+                continue
+            if blocks2[i]['file'] != blocks2[j]['file']:
+                continue
+            name2 = blocks2_name[j]
+            if name2 == None:
+                continue
+            if remove_whitespace(name1) == remove_whitespace(name2) \
+                and remove_whitespace(blocks2[i]['header']) != remove_whitespace(blocks2[j]['header']):
+                same_named[i],same_named[j] = True, True
+
+    visited = [False] * len(blocks2)
+    target_headers = []
+    for inner_header in inner_headers:
+        block_name1 = extract_name(inner_header)
+        for i in range(len(blocks2)):
+            if visited[i]:
+                continue
+            if same_named[i]:
+                block_header = blocks2[i]['header']
+                if isblockpara(inner_header, block_header):
+                    target_headers.append(blocks2[i]['header'])
+                    visited[i] = True
+            else:
+                block_name2 = blocks2_name[i]
+                if block_name1 != None and block_name2 != None:
+                    if isblockpara(block_name1, block_name2):
+                        target_headers.append(blocks2[i]['header'])
+                        visited[i] = True
+                else:
+                    block_header = blocks2[i]['header']
+                    if isblockpara(inner_header, block_header):
+                        target_headers.append(blocks2[i]['header'])
+                        visited[i] = True
+    return target_headers
+
+
+def clear(file_string1,inner_header):
+    visited = 0
+    tmp_file_string1 = file_string1.split("\n")
+    for i,line in enumerate(tmp_file_string1):
+        if line not in inner_header:
+            tmp_file_string1[i] = " "*len(line)
+        else:
+            visited = 1
+            for j,line in enumerate(tmp_file_string1):
+                if j <= i :  continue
+                if line in inner_header:
+                    visited = visited + 1
+                    continue
+                if re.match(header_re,line) and visited == len(inner_header):
+                    for k,line in enumerate(tmp_file_string1):
+                        if k < j :  continue
+                        tmp_file_string1[k] = " "*len(line)
+                    return tmp_file_string1
+            return tmp_file_string1
+    return None
+
+
+def block_result(cfile1,
+                patch,
+                cfile2,
+                use_docker,
+                MATCHER_ID,
+                TREE_GENERATOR_ID,
+                mapping_dic):
+
+    patch = copy.deepcopy(patch)
+    if len(read_patch(patch["patch"])) != 0:
+        changed_header = read_patch(patch["patch"])[0]
+    else:
+        changed_header = patch["header"]
+    inner_header = []
+    for line in patch["patch"]:
+        if len(line) > 0 and line[0] == "-" and re.match(header_re,line[1:]):
+            inner_header.append(line[1:])
+    inner_header = set(inner_header)
+    inner_header.add(changed_header)
+    file_string1 = clear(cfile1,inner_header)
+    file_string1 = "\n".join(file_string1)
+    with tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as file1, \
+        tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.patch') as patchfile1:
+            file1.write(file_string1)
+            file1.flush()
+            patchfile1.write("\n".join(patch["patch"])+"\n")
+            patchfile1.flush()
+            output11_ = subprocess.run(["patch",file1.name,patchfile1.name,"--output=-"],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+            file_string1_ = output11_.stdout.decode()
+    file_string2 = cfile2
+
+
+    blocks2 = extract_block(file_string2.split("\n"),file_name="2")
+
+    target_header = collect_mirrored_blocks(inner_header,blocks2)
+
+
+    if len(target_header) != len(inner_header):
+        with tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.cc') as file1, \
+        tempfile.NamedTemporaryFile(delete=True, mode='w', suffix='.patch') as patchfile1:
+            file1.write(cfile1)
+            file1.flush()
+            patchfile1.write("\n".join(patch["patch"])+"\n")
+            patchfile1.flush()
+            output11_ = subprocess.run(["patch",file1.name,patchfile1.name,"--output=-"],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+            file_string1_ = output11_.stdout.decode()
+        file_string2 = gen_result(cfile1,file_string2,file_string1_,mapping_dic,use_docker,MATCHER_ID,TREE_GENERATOR_ID)
+        return file_string2
+
+    match = [file_string1.strip()+"\n",
+            file_string1_.strip()+"\n",
+            "\n".join(clear(file_string2,target_header)).strip()+"\n"
+            ]
+    block2_ = gen_result(match[0],match[2],match[1],mapping_dic,use_docker,MATCHER_ID,TREE_GENERATOR_ID)
+    file_string2 = file_string2.replace(match[2],block2_)
+    return file_string2
